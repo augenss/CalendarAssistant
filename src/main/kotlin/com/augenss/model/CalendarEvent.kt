ea2514717f650cfeb59440a2f88bb3c90bda47aa
augenss <contact@augenss.com>
2021-03-14 01:10:26 +0100

    Add initial model

---
+++ src/main/kotlin/com/augenss/model/CalendarEvent.kt
@@ -0,0 +1,5 @@
+package com.augenss.model
+
+data class CalendarEvent(
+    private val id: Int
+)
---
+++ src/main/kotlin/com/augenss/model/User.kt
@@ -0,0 +1,20 @@
+package com.augenss.model
+
+import kotlinx.serialization.Serializable
+import org.jetbrains.exposed.dao.id.IntIdTable
+
+@Serializable
+data class User (
+    val id: Int,
+    val username: String,
+    val password: String,
+    val name: String,
+//    val surname: String?
+)
+
+object Users: IntIdTable() {
+    val username = varchar("username", 50)
+    val password = varchar("password", 50)
+    val name = varchar("name", 50)
+    val surname = varchar("surname", 50)
+}
